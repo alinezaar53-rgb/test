129122e537b1cfdbbc5c4f89d522373fa6796bd7
unknown <you@example.com>
2026-07-12 16:14:10 -0700

    add Jwally Method

--- aa.dart
+++ aa.dart
@@ -1,11 +1,27 @@
-vodi main(){}
+vodi main(){
+    paymentMethod paymentMethod
+    paymentMethod=new JwallyMethod();
+    paymentMethod.pay(20.0);
+    paymentMethod=new JaibMethod
+    paymentMethod.pay (30.0);
+}
 abstract class paymentMethod{
-    vodi pay (double amount);
+    bool pay (double amount);
 }
 class JaibMethod implementMethod paymentMethod{
     @override 
-    void pay (double amount){
-        print ("paying ${amount}with Jaib Method");
+    bool  pay (double amount){
+        print ("paying $amount using Jaib Method");
+        return true;
+    }
+}
+
+class JwallyMethod implementMethod paymentMethod{
+    @override 
+    bool  pay (double amount){
+        print ("paying $amount using Jwally Method");
+                return true;
+
     }
 }
  
