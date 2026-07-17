b9c46207b997f82c5b9fab628fa7ddcde7f2e49b
unknown <you@example.com>
2026-07-17 09:46:23 -0700

    first commit

---
+++ main.dart
@@ -0,0 +1,20 @@
+void main() {
+  print("hello werde ");
+ Math math;
+// print(math.sum(5,10));
+// print(math.sum("ali",10.toString()));
+ //print(math.sum("ali ","Ahmmed"));
+ math =new Math.test("A");
+}
+class Math{
+  Math (){
+    print ("Math started");
+  }
+  Math.test(a){
+    print("constractor wiht test");
+    print(a);
+  }
+  sum(a,b){
+    return a+b;
+  }
+}
